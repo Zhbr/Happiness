bd53b298b2d3ad6a88ef76acc9f9684822d1c7d5
Yaroslav Karmakov <YaroslavKarmkaov@Yaroslavs-MacBook-Pro.local>
2015-06-23 10:34:45 +0600

    first commit

---
+++ Happiness/HappinessViewController.swift
@@ -0,0 +1,16 @@
+//
+//  HappinessViewController.swift
+//  Happiness
+//
+//  Created by Yaroslav Karmakov on 6/18/15.
+//  Copyright (c) 2015 AnyWayIGo. All rights reserved.
+//
+
+import UIKit
+
+
+class HappinessViewController: UIViewController
+{
+
+
+}
